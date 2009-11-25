79afab39674b13aed96fa7f96f1646b1e1b49b4d
hm2k <hm2k@af9edebb-e53e-4d49-a068-920743921f27>
2009-11-25 11:44:31 +0000

    corrected documentation; removed file headers and vim/cvs comments; removed Colin Viebrock's contact information from the README
    git-svn-id: svn+ssh://svn.code.sf.net/p/opensrs-php/code@6 af9edebb-e53e-4d49-a068-920743921f27

--- opensrs-php/openSRS_base.php
+++ opensrs-php/openSRS_base.php
@@ -1,38 +1,4 @@
 <?php
-/* 
- **************************************************************************
- *
- * OpenSRS-PHP
- *
- * Copyright (C) 2000-2004 Colin Viebrock
- *
- * Version 2.8.0
- *   15-Dec-2004
- *
- **************************************************************************
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.   
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- **************************************************************************
- *
- * vim: set expandtab tabstop=4 shiftwidth=4:
- * $Id: openSRS_base.php,v 1.16.2.4 2004/12/07 20:27:22 cviebrock Exp $
- *
- **************************************************************************
- */
-
 
 # assuming that the PEAR directory is within your path, otherwise change the path here
 
